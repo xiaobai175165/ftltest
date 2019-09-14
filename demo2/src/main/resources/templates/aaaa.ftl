12577a1e51e42022348968d6f6c490fabea818c2
张迪 <1738175165@qq.com>
2019-09-14 16:23:06 +0800

    删除

--- demo2/src/main/resources/templates/aaaa.ftl
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
-<head>
-    <meta charset="utf-8"/>
-    <title>index</title>
-</head>
-<body>
-<h1 align="center">Hello:</h1>
-</body>
-</html>
---
+++ ftltest/src/test/java/com/kgc/ftltest/FtltestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kgc.ftltest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FtltestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
